831455df04674c205fa469f6649e45425b62e019
Dacheng Gao <successgdc@gmail.com>
2025-04-05 16:40:47 +0800

    update rust edition to 2024

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     for n in 0..20 {
-        println!("{n:2},{:5}", f(n))
+        println!("fib({n}) => {}", fib(n))
     }
 }
 
-fn f(n: i32) -> i32 {
+fn fib(n: i32) -> i32 {
     if n == 0 {
         0
     } else if n == 1 {
         1
     } else {
-        f(n - 1) + f(n - 2)
+        fib(n - 1) + fib(n - 2)
     }
 }
